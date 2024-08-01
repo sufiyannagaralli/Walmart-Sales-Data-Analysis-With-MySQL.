9c840f2e1a9d23dc03e7d646d398706d88b38c21
Md Sufiyan Nagaralli <94040413+sufiyannagaralli@users.noreply.github.com>
2024-08-01 17:51:04 +0530

    Update Walmart_kaggle_dataset.sql

--- Walmart_kaggle_dataset.sql
+++ Walmart_kaggle_dataset.sql
@@ -45,11 +45,10 @@ ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);
 
 UPDATE sales
 SET time_of_day = (
-    CASE 
-        WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
-        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
-        ELSE "Evening"
-    END
+    CASE WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
+         WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
+         ELSE "Evening"
+     END
 );
 
 
